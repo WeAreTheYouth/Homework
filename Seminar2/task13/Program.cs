9080121b37f895ec26d848acc58c925a493b3108
WeAreTheYouth <open8your5mind@gmail.com>
2022-09-16 22:11:10 +0300

    task13 done

---
+++ Seminar2/task13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine());
+if (number > 99)
+{
+    char threeNumber = number.ToString()[2];
+    Console.WriteLine($"Третья цифра {threeNumber}");
+}
+else 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
