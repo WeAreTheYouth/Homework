54f5d10e5bbb97cfe1689db15b809524984230ad
WeAreTheYouth <open8your5mind@gmail.com>
2022-09-16 22:16:15 +0300

    task15 done

--- Seminar2/task15/Program.cs
+++ Seminar2/task15/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+if (number > 0 && number < 6)
+{
+    Console.WriteLine("Будний день");
+}
+else if (number > 5 && number < 8)
+{
+    Console.WriteLine("Выходной день");
+}
