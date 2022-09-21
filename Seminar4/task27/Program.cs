1e9a3b68ebabfe3f410aa3ae2a454bd46f6659da
WeAreTheYouth <open8your5mind@gmail.com>
2022-09-22 02:04:54 +0300

    task29 done

--- Seminar4/task27/Program.cs
+++ Seminar4/task27/Program.cs
@@ -1,3 +1,18 @@
 ﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
+
+int GetSum(int num)
+{
+    while (num > 0)
+    {
+        int digit = num % 10;
+        sum = sum + digit;
+        num = num / 10;
+    }
+    return sum;
+}
+
+Console.WriteLine($"{GetSum(num)}");
---
+++ Seminar4/task29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Пользователь вводит 8 чисел. Поместите их в массив и распечатайте.
+
+int[] CreateArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите элемент массива под индексом {i}: ");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.WriteLine($"[{String.Join(", ", arr)}]");
+}
+
+PrintArray(CreateArray());
