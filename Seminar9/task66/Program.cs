f2da1e4d11fd2e9d0a57fd0a5598f9c2072fc217
WeAreTheYouth <open8your5mind@gmail.com>
2022-10-18 19:32:40 +0300

    seminar9

--- Seminar9/task66/Program.cs
+++ Seminar9/task66/Program.cs
@@ -1,2 +1,20 @@
 ﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму 
 // натуральных элементов в промежутке от M до N.
+
+Console.Write("Введите M: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите N: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма от M до N = {GetSum(num1, num2)}");
+
+int GetSum(int min, int max)
+{
+    int sum = 0;
+    if (min == max)
+    {
+        return min;
+    }
+    sum += max;
+    return GetSum(min, max - 1) + sum;
+
+}
